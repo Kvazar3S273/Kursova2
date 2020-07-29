b56ac64bca706d4c5c6793ba4ebcb81c72270536
Natalia-77 <vlasnata80@gmail.com>
2020-07-30 00:36:13 +0300

    file-change

--- Kursova2/Kursova2/Source.cpp
+++ Kursova2/Kursova2/Source.cpp
@@ -5,18 +5,25 @@ void main()
 	Ukr();
 	Candidate c1;
 	Candidate c2;
+	Candidate c3;
+	Candidate c4;
 	vector<Candidate> can;
 	vector<Candidate>::iterator it;
 
-	c1.CreateCandidate(14, 1, 2019, 1, "Корейці України", "Кім", "Чонг", "Ха", 13, 50, 15954);
-	c2.CreateCandidate(18, 10, 2018, 2, "Зануди", "Муденко", "Леонард", "Кирилович", 11, 1, 4);
+	c1.CreateCandidate(14, 1, 2019, 2, "Корейці України", "Кім", "Чонг", "Ха", 13, 50, 15954);
+	c2.CreateCandidate(18, 10, 2018, 3, "Зануди", "Муденко", "Леонард", "Кирилович", 11, 1, 4);
+	c3.CreateCandidate(18, 10, 2018, 4, "Петрики", "Лапки", "Гусячі", "Кирилович", 31, 1, 4);
+	c4.CreateCandidate(20, 20, 1900, 5, "Янукович", "Віктор", "Федорович", "Лижи", 25, 22, 8);
 
 	//read from file	
-	fstream ifs("sss.txt",ios_base::in);
+	fstream ifs("new.txt",ios_base::in);
 	copy(istream_iterator<Candidate>(ifs),istream_iterator<Candidate>(),back_inserter(can));
-
+	ifs.close();
 	can.push_back(c1);
 	can.push_back(c2);
+	can.push_back(c3);
+	can.push_back(c4);
+
 	
 
 	//out to console vector
@@ -38,10 +45,10 @@ void main()
 
 
 	//delete candidate to position
-	/*int pos;
+	int pos;
 	cout << "Enter position for delete";
 	cin >> pos;
-	can.erase(can.begin() + pos - 1);*/
+	can.erase(can.begin() + pos - 1);
 
 	 //delete to argument
 	/* can.erase(remove_if(can.begin(),can.end(), [temp](const Candidate& c) { return c.district ==temp;}), end(can));*/
@@ -49,17 +56,17 @@ void main()
 	
 	
 	 //out to console vector after operations.
-	//TableCap();
-	//for (it = can.begin(); it < can.end(); ++it)
-	//{
-	//	cout << *it << endl;//out to console vector.
-	//}
-	//cout << endl;
+	TableCap();
+	for (it = can.begin(); it < can.end(); ++it)
+	{
+		cout << *it << endl;//out to console vector.
+	}
+	cout << endl;
 
 
 	//save to file
-	/*ofstream out("sss.txt", ios_base::trunc || ios_base::out);
-	copy(can.begin(), can.end(), ostream_iterator<Candidate>(out));*/
-
-
+	ofstream out("new.txt", ios_base::trunc || ios_base::out);
+	copy(can.begin(), can.end(), ostream_iterator<Candidate>(out));
+	out.close();
+	
 }
